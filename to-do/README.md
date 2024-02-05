3ddf88ef1f3c1ae9c754f3b97b65a09926e8925c
a-code-writer <124473111+a-code-writer@users.noreply.github.com>
2024-02-05 08:35:42 -0500

    put to-do app into different folder because git wasnt tracking it, no .git file in it this time

---
+++ to-do/README.md
@@ -0,0 +1,3 @@
+# to-do-project
+deleted the git file, ro-do app connected to Odin project repo
+asda
---
+++ to-do/src/index.js
@@ -0,0 +1,269 @@
+let projects = [];
+let taskList = [];
+let projectsTab = document.getElementById('projectsTab') //sidebar
+let addTask = document.getElementById('add-task') 
+
+function toDo(name, dueDate, priority, projParent){ //to-do object
+    return {
+        name,
+        dueDate,
+        priority,
+        projParent 
+    }
+}
+
+
+//project form
+let projName = document.createElement('input');
+projName.setAttribute('placeholder', 'Project Name')
+
+let addBtn = document.createElement('button')
+addBtn.setAttribute('type', 'submit');
+addBtn.textContent = 'Add';
+
+let cancelBtn = document.createElement('button');
+cancelBtn.setAttribute('type', 'button')
+cancelBtn.textContent = "Cancel";
+
+//Appends to project form to DOM
+let form = document.createElement('form'); 
+form.appendChild(projName);
+form.appendChild(addBtn);
+form.appendChild(cancelBtn);
+
+//add project btn
+let projBtn = document.getElementById('add-btn')
+projBtn.addEventListener('click', () => {
+    document.getElementById('content').appendChild(form);
+})
+
+
+
+//cancel button
+cancelBtn.addEventListener('click', () =>{
+    form.remove();
+})
+
+
+
+//To-do Name
+let taskNameInput = document.createElement('input') 
+taskNameInput.setAttribute('placeholder', "Task Name");
+
+//submit to-do btn
+let taskSubmit = document.createElement('button');
+taskSubmit.setAttribute('type', 'submit');
+taskSubmit.textContent = "Submit Task"
+
+//to-do cancel
+let taskCancel = document.createElement('button');
+taskCancel.textContent = "Cancel"
+taskCancel.addEventListener('click', () => {
+    taskForm.remove()
+})
+
+//to-do priority checkbox
+let taskPriority = document.createElement('input')
+taskPriority.setAttribute('type', 'checkbox')
+taskPriority.setAttribute('id', 'priority-check')
+
+//label for priority checkbox
+let taskPriorityLabel = document.createElement('label');
+taskPriorityLabel.setAttribute('for', 'priority-check')
+taskPriorityLabel.textContent = "Is this a priority task?"
+
+//to-do calender selector
+let taskDuedate = document.createElement('input')
+taskDuedate.setAttribute('type', 'date')
+
+let dueDateLabel = document.createElement('label');
+dueDateLabel.textContent = "When is this due?"
+
+let taskProject = document.createElement('select')
+let taskProjectlabel = document.createElement('label');
+taskProjectlabel.textContent = "Does this belong in a project?"
+
+
+//to-do form is assembled
+let taskForm = document.createElement('form')
+taskForm.appendChild(taskNameInput)
+taskForm.appendChild(taskPriority)
+taskForm.appendChild(taskPriorityLabel)
+taskForm.appendChild(taskDuedate)
+taskForm.appendChild(dueDateLabel)
+taskForm.appendChild(taskProject)
+taskForm.appendChild(taskProjectlabel)
+taskForm.appendChild(taskSubmit)
+taskForm.appendChild(taskCancel)
+
+//add task btn (adds to-do form to DOM)
+addTask.addEventListener('click', () => {
+    document.getElementById('content').appendChild(taskForm)
+})
+
+//submit project button
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    projects.push(projName.value); //add input to list
+    projectsTab.innerHTML = ""; //resets sidebar content
+    for(let i = 0; i < projects.length; i++){ //adds all current projects in array to sidebar
+        
+        let projLi = document.createElement('li') 
+        projLi.innerText = projects[i] 
+        projLi.setAttribute('index', i); //How I will be able to choose and edit specific projects
+
+        let menu = document.createElement('img'); //3 dots at end of project div
+        menu.setAttribute('src', '../3-dots.png')
+        menu.setAttribute('index', i) //making sure each menu button corresponds to its matching project
+        menu.addEventListener('click', () => {
+            let adjLi = document.querySelector(`li[index="${i}"]`)//adjLi is the corresponding project
+            if(!adjLi.nextElementSibling || adjLi.nextElementSibling.tagName !== "DIV"){ //checks if projLi has div sibling already
+                let menuBtns = document.createElement('div');// all buttons will be stored in a seperate div
+                menuBtns.setAttribute('index', i)
+
+                //inside menu button
+                let editBtn = document.createElement('button')
+                editBtn.innerText = "Edit";
+                editBtn.addEventListener('click', () => {
+                    let editProjForm = document.createElement('form');
+                    let updateBtn = document.createElement('button')
+                    let cancelNameBtn = document.createElement('button')
+
+                    updateBtn.textContent = "Update";
+                    updateBtn.setAttribute("type", "submit")
+                    cancelNameBtn.textContent = "Cancel"
+
+                    //button inside of Edit button
+                    cancelNameBtn.addEventListener("click", () => {
+                        editProjForm.remove()
+                    })
+
+                    //inside of edit button, will change the projects Name
+                    let updateNameInput = document.createElement('input')
+                    updateNameInput.setAttribute('placeholder', "Updated name")
+
+                    editProjForm.appendChild(updateBtn);
+                    editProjForm.appendChild(cancelNameBtn);
+                    editProjForm.appendChild(updateNameInput);
+                    editProjForm.addEventListener('submit', (event) => {
+                        event.preventDefault();
+                        projects[i] = updateNameInput.value;
+                        adjLi.textContent = updateNameInput.value
+                        projLi.appendChild(menu)
+                    })
+                    menuBtns.remove()
+                    projectsTab.insertBefore(editProjForm, adjLi.nextElementSibling)
+                })
+
+                //inside of menu button
+                let deleteBtn = document.createElement('button')
+                deleteBtn.innerText = 'Delete';
+                deleteBtn.addEventListener('click', () => {
+                    projects.splice(i)
+                    document.querySelector(`li[index="${i}"]`).remove()
+                    document.querySelector(`div[index="${i}"]`).remove()
+                })
+
+                //inside of menu button
+                let cancelbutton = document.createElement('button');
+                cancelbutton.innerText = 'Cancel'
+                cancelbutton.addEventListener('click', () =>{
+                    document.querySelector(`div[index="${i}"]`).remove()
+                })
+
+                //buttons put inside of div
+                menuBtns.appendChild(editBtn)
+                menuBtns.appendChild(deleteBtn)
+                menuBtns.appendChild(cancelbutton);
+                
+                //button div is added before current projects next sibling ie: right after it   
+                projectsTab.insertBefore(menuBtns, adjLi.nextSibling) 
+            }
+        })
+        projLi.appendChild(menu)
+        projectsTab.appendChild(projLi)
+    }
+
+    //adding project options to the to-do form(dropdown menu)
+    taskProject.innerHTML = '<option></option>';
+    projects.forEach(project => {
+        const option = document.createElement('option');
+        option.text = project;
+        option.value = project;
+        taskProject.appendChild(option);
+    })
+
+    form.remove()
+})
+
+taskForm.addEventListener('submit',(event)=> {
+    event.preventDefault();
+    const newToDo = toDo(taskNameInput.value, taskDuedate.value, taskPriority.checked, taskProject.value)
+    taskList.push(newToDo);
+    document.getElementById('content').innerHTML = ""
+
+    //use list to create DOM to-do elements
+    for(let i = 0; i < taskList.length; i++){
+        let container = document.createElement('li') //container div for to-do
+        container.setAttribute('index', i) 
+        let containerLeft = document.createElement('div')
+        containerLeft.setAttribute('style', 'display: flex')
+        let containerRight = document.createElement('div')
+        containerRight.setAttribute('style', 'display: flex; align-items: center')
+
+        let toDoCheck = document.createElement('input')
+        toDoCheck.setAttribute('type', 'checkbox')
+
+        let toDoName = document.createElement('p');
+        toDoName.textContent = taskList[i].name;
+
+        let toDoDate = document.createElement('p')
+        toDoDate.textContent = taskList[i].dueDate;
+
+        let toDoMenu = document.createElement('img');
+        toDoMenu.setAttribute('src', '../3-dots.png') 
+        toDoMenu.setAttribute('style', 'max-height: 20px; max-width: 20px')
+        toDoMenu.setAttribute('index', i)
+        toDoMenu.addEventListener('click', () => {
+            if(!container.nextElementSibling || container.nextElementSibling.tagName !== "DIV"){
+                let toDoBtnsDiv = document.createElement('div') //div which will contain all 3 menu btns for to-do
+                toDoBtnsDiv.setAttribute('style', 'display: flex; justify-content: end;')
+                let toDoEdit = document.createElement('button')
+                toDoEdit.textContent = "Edit"
+                let toDoDelete = document.createElement('button')
+                toDoDelete.textContent = "Delete";
+                toDoDelete.addEventListener('click', () => {
+
+                })
+                let toDoCancel = document.createElement('button')
+                toDoCancel.textContent = 'Cancel'
+                toDoCancel.addEventListener('click', () => {
+                    toDoBtnsDiv.remove()
+                })
+                
+                
+                
+                toDoBtnsDiv.appendChild(toDoEdit)
+                toDoBtnsDiv.appendChild(toDoDelete)
+                toDoBtnsDiv.appendChild(toDoCancel)
+                document.getElementById('content').insertBefore(toDoBtnsDiv, container.nextElementSibling)
+            }
+        })
+
+
+        containerLeft.appendChild(toDoCheck);
+        containerLeft.appendChild(toDoName);
+
+        containerRight.appendChild(toDoDate)
+        containerRight.appendChild(toDoMenu);
+
+        container.appendChild(containerLeft);
+        container.appendChild(containerRight);
+        container.setAttribute('style', ' display: flex; justify-content: space-between');
+
+        document.getElementById('content').appendChild(container)
+    }
+
+    taskForm.remove()
+})
+
